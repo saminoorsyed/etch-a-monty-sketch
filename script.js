ade303ffb8fa763a2dd2b661999aac55b5550ccd
Sami <saminoorsyed@gmail.com>
2022-08-23 22:36:29 -0400

    Added event listener to range slider

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const container = document.querySelector('container');
-
+const container = document.querySelector('.container');
+const slider = document.querySelector('.slider');
 // generate grid dimensions based on range slider
-function genGridDimensions(){
+let rangeDrag = false;
+let dimension;
 
+function genDimensions(){
+    // if the mouse is not clicked down, do not run the function when mouse is moving over div
+    if (!rangeDrag) return;
+    // the value of the range slider from 1 to 128
+    dimension = this.value;
+    // populate the container div with container elements
 }
+
+slider.addEventListener('change', genDimensions);
+slider.addEventListener('mousemove', genDimensions);
+
+slider.addEventListener('mousedown', () => rangeDrag = true);
+slider.addEventListener('mouseup', () => rangeDrag = false);
+slider.addEventListener('mouseout', () => rangeDrag = false);
